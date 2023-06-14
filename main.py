ca3842db71a217edcf708129e065d0d53d02e03b
petter584 <vitalijus.serpytis@teliacompany.com>
2023-06-14 22:39:21 +0300

    added monitoring and changed pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import requests
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from prometheus_flask_exporter import PrometheusMetrics
 
 def get_random_quote():
     url = "https://zenquotes.io/api/random"
@@ -12,21 +13,30 @@ def get_random_quote():
 
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+# static information as metric
+metrics.info('app_info', 'Application info', version='1.0.3')
+
+
 
 limiter = Limiter(
     get_remote_address,
     app=app,
     storage_uri="memory://",
 )
+@app.route('/')
+def main():
+    return "<h1>Landing page</h1>"
 
 @app.route('/hello', methods=['GET'])
-@limiter.limit("5 per minute")
+@limiter.limit("10 per minute")
 def hello():
     return jsonify({"greeting": "hello world!"})
 
 
 @app.route('/quote', methods=['GET'])
-@limiter.limit("4 per minute")
+@limiter.limit("5 per minute")
 def quote():
     quote = get_random_quote()
     return jsonify({"quote": quote})
